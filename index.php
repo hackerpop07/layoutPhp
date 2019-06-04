64f7e3a116f8551e5d31e9cfc4287933f5c4021a
hackerpop07 <hackerpop07@gmail.com>
2019-06-04 15:34:54 +0700

    php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<!doctype html>
+<html lang="vn">
+<head>
+</head>
+<body>
+<div class="container">
+    <header><?php  require "header.php";  ?></header>
+    <nav><?php require "nav.php";  ?></nav>
+    <article> <?php require "content.php"; ?></article>
+    <footer><?php require "footer.php";?></footer>
+</div>
+</body>
+</html>
